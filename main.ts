6d70c2386e4b2682abb752b8a2325bb1e4d38003
Wang <wang.xy@111.com>
2023-03-29 23:45:14 +0800

    更改项目名称，添加设置，添加侧栏按钮

--- main.ts
+++ main.ts
@@ -1,30 +1,85 @@
-import { Plugin, MarkdownView } from "obsidian";
+import { App,Plugin, MarkdownView,Notice,PluginSettingTab,Setting } from "obsidian";
 import { MarkdownIndex } from "./MarkdownIndex";
 
-export default class titleIndexPlugin extends Plugin {
-  async onload() {
-    this.addCommand({
-      id: "add-markdown-index",
-      name: "add-markdown-index",
-      checkCallback: (checking: boolean) => {
-        const markdownView =
-          this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          if (!checking) {
-            const editor = markdownView.editor;
-            const cursor = editor.getCursor();
+interface indexFormattingPluginSettings{
+    testSetting1: string;
+}
 
-            const lines = editor.getValue().split("\n");
-            const markdownIndex = new MarkdownIndex();
-            const newlines = markdownIndex.addMarkdownIndex(lines);
-            editor.setValue(newlines.join("\n"));
+const DEFAULT_SETTINGS: indexFormattingPluginSettings = {
+    testSetting1: 'test default setting',
+}
 
-            editor.setCursor(cursor);
-          }
-          return true;
-        }
-        return false;
-      },
-    });
-  }
+
+export default class indexFormattingPlugin extends Plugin {
+    settings:indexFormattingPluginSettings
+    async onload() {
+        await this.loadSettings();
+        this.addSettingTab(new SampleSettingTab(this.app, this));
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Format this note', (evt: MouseEvent) => {
+            const markdownView =this.app.workspace.getActiveViewOfType(MarkdownView);
+            if(markdownView){
+                this.format_a_note(markdownView);
+            }
+        });
+        this.addCommand({
+            id: "obsidian-index-formatting-format_this_note",
+            name: "Format this note",
+            checkCallback: (checking: boolean) => {
+                const markdownView =this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (!markdownView) {
+                    return false;
+                }
+                else{
+                    if (!checking) {
+                        this.format_a_note(markdownView);
+                    }
+                    return true;
+                }
+            },
+        });
+    }
+
+    format_a_note(markdownView:MarkdownView){
+        new Notice("Index Formatting: This note is formatted.");
+        const editor = markdownView.editor;
+        const cursor = editor.getCursor();
+        const lines = editor.getValue().split("\n");
+        const markdownIndex = new MarkdownIndex();
+        const newlines = markdownIndex.addMarkdownIndex(lines);
+        editor.setValue(newlines.join("\n"));
+        editor.setCursor(cursor);
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+}
+
+class SampleSettingTab extends PluginSettingTab {
+    plugin: indexFormattingPlugin;
+    constructor(app: App, plugin: indexFormattingPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const {containerEl} = this;
+        containerEl.empty();
+        containerEl.createEl('h2', {text: 'title index'});
+        new Setting(containerEl)
+            .setName('a test setting')
+            .setDesc('just test')
+            .addText(text => text
+                .setPlaceholder('enter text')
+                .setValue(this.plugin.settings.testSetting1)
+                .onChange(async (value) => {
+                    console.log('Secret: ' + value);
+                    this.plugin.settings.testSetting1 = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
